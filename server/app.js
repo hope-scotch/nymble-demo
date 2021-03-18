b04a0eba6f44bdc0b5c231c6d0b1b0af63cc74bd
Sayantan Biswas <f20180081@hyderabad.bits-pilani.ac.in>
2021-03-18 11:50:23 +0530

    Updated

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('../config/db')
+const path = require('path')
 
 const userRoute = require('./routes/api/users.js')
 const expenseRoute = require('./routes/api/expense.js')
@@ -18,5 +19,13 @@ app.use(express.json({ extended: false }))
 app.use(userRoute)
 app.use(expenseRoute)
 app.use(authRoute)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
